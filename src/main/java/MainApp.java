530db755975c1e33ed4e04a705fdded9c9a5dd51
Mikhail Gaivola <starskvim@mail.ru>
2021-12-19 21:01:15 +0300

    Added Sync Renaming in B

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -5,9 +5,12 @@ import org.apache.commons.io.FilenameUtils;
 import java.io.*;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.DecimalFormat;
+
+import java.nio.file.StandardCopyOption;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
@@ -21,31 +24,34 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-//        String folder1 = "F:\\test\\folder1";
-//        String folder2 = "F:\\test\\folder2";
+        String folder1 = "F:\\test\\folder1";
+        String folder2 = "F:\\test\\folder2";
 
-        String folder1 = "F:\\[3D PRINT]\\Модели\\[Patreon]\\[Figure]";
-        String folder2 = "D:\\[Patreon]\\[Figure]";
+//        String folder1 = "F:\\[3D PRINT]\\Модели\\[Patreon]\\[Figure]";
+//        String folder2 = "D:\\[Patreon]\\[Figure]";
 
         //syncStartMethodA(folder1, folder2);
         syncStartMethodB(folder1, folder2);
         //syncStartMethodC(folder1, folder2);
         //System.out.println(" done ");
 
+//        try {
+//            Collection<File> test = startScan(folder1);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
 
         //String fileForMD5 = "F:\\test\\folder1\\11\\1121\\112131\\okas ore glow.zip";
         //System.out.println(getMD5file(fileForMD5));
     }
 
-    public static void syncStartMethodA(String folder1, String folder2){
+    public static void syncStartMethodA(String folder1, String folder2) {
 
-        try {
-            folder1list.addAll(startScan(folder1));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        for(File file: folder1list){
+        folder1list.addAll(startScan(folder1));
+
+        for (File file : folder1list) {
             try {
 
                 String absPathCurrentFile = file.getAbsolutePath();
@@ -54,7 +60,7 @@ public class MainApp {
                 String pathNewFile = pathCurrentFile.replace(folder1, folder2);
                 String absPathNewFile = absPathCurrentFile.replace(folder1, folder2);
 
-                if (Files.exists(Paths.get(pathNewFile)) ) {
+                if (Files.exists(Paths.get(pathNewFile))) {
                     System.out.println("yes");
 
                     if (!Files.isRegularFile(Paths.get(absPathNewFile))) {
@@ -63,7 +69,7 @@ public class MainApp {
                         System.out.println("skipped " + absPathNewFile);
                     }
 
-                }else {
+                } else {
                     System.out.println("no");
 
                     try {
@@ -85,49 +91,82 @@ public class MainApp {
         }
     }
 
-    public static void syncStartMethodB(String folder1, String folder2){
+    public static void syncStartMethodB(String folder1, String folder2) {
 
         syncStartMethodA(folder1, folder2);
 
-        try {
+        folder2list.addAll(startScan(folder2));
+        if (!folder2list.isEmpty()) {
+            syncRenamingFiles(folder1list, folder2list);
+            folder2list.clear();
             folder2list.addAll(startScan(folder2));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
-        for (File file: folder1list){
+        for (File file : folder1list) {
             currentFileABSpath.add(file.getAbsolutePath());
         }
 
-        for(File file: folder2list){
+        for (File file : folder2list) {
 
-            if (!currentFileABSpath.contains(file.getAbsolutePath().replace(folder2, folder1))){
+            if (!currentFileABSpath.contains(file.getAbsolutePath().replace(folder2, folder1))) {
 
                 File fileFolder = new File(file.getParent());
-                if(file.delete()){
+                if (file.delete()) {
                     System.out.println(file.getName() + " - удален");
                 } else {
                     System.out.println(file.getName() + " - не удален");
                 }
-                if (fileFolder.delete()){
+                if (fileFolder.delete()) {
                     System.out.println(file.getParent() + " - папка удалена");
-                } else  {
+                } else {
                     System.out.println(file.getParent() + " - папка не пуста");
                 }
 
             }
         }
 
+
     }
 
-    public static void syncStartMethodC(String folder1, String folder2){
+    public static void syncStartMethodC(String folder1, String folder2) {
         syncStartMethodA(folder1, folder2);
         folder1list.clear();
         folder2list.clear();
         syncStartMethodA(folder2, folder1);
     }
 
-    public static String getMD5file(String path){
+    public static void syncRenamingFiles(HashSet<File> folder1listToMD5, HashSet<File> folder2listToMD5) {
+
+
+        HashMap<String, File> folder1FilesMap = new HashMap<>();
+        for (File file : folder1listToMD5) {
+            folder1FilesMap.put(getMD5file(file.getAbsolutePath()), file);
+        }
+        for (File targetFile : folder2listToMD5) {
+
+            String md5TargetFile = getMD5file(targetFile.getAbsolutePath());
+
+            if (folder1FilesMap.containsKey(md5TargetFile)) {
+                File currentFile = folder1FilesMap.get(md5TargetFile);
+                if (!currentFile.getName().equals(targetFile.getName())) {
+
+                    System.out.println(targetFile.getParent() + "\\" + currentFile.getName());
+
+                    Path targetPath = targetFile.toPath();
+                    Path targetPathRename = Paths.get(targetFile.getParent() + "\\" + currentFile.getName());
+
+                    try {
+                        Files.move(targetPath, targetPathRename, StandardCopyOption.REPLACE_EXISTING);
+                        System.out.println("rename ok");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static String getMD5file(String path) {
         String md5 = "";
         try {
             try (InputStream is = Files.newInputStream(Paths.get(path))) {
@@ -138,26 +177,31 @@ public class MainApp {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
         return md5;
     }
 
-    public static Collection<File> startScan(String adress) throws IOException {
+    public static Collection<File> startScan(String adress) {
 
         long start = System.currentTimeMillis();
 
         File adres = new File(adress);
 
-        Collection<File> files = FileUtils.streamFiles(adres, true, null).collect(Collectors.toList());
 
-//        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
-//        for (File file : files) {
-//
-//            System.out.println(file.getPath() + "--------" + file.getParent() + "--------" + file.getParentFile().getName() + "--------" + FilenameUtils.getExtension(file.getName()));
-//
-//        }
+        Collection<File> files = null;
+        try {
+            files = FileUtils.streamFiles(adres, true, null).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (File file : files) {
+
+            System.out.println(file.getPath() + "--------" + file.getParent() + "--------" + file.getParentFile() + "--------" + FilenameUtils.getExtension(file.getName()));
+
+        }
 
         long fin = System.currentTimeMillis();
         System.out.println("scan time " + (fin - start));
@@ -173,10 +217,10 @@ public class MainApp {
             destChannel = new FileOutputStream(dest).getChannel();
             destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
         } finally {
-            if(sourceChannel != null){
+            if (sourceChannel != null) {
                 sourceChannel.close();
             }
-            if (destChannel != null){
+            if (destChannel != null) {
                 destChannel.close();
             }
         }
@@ -194,11 +238,11 @@ public class MainApp {
                 os.write(buffer, 0, length);
             }
         } finally {
-            if(is != null){
-            is.close();
+            if (is != null) {
+                is.close();
             }
-            if (os != null){
-            os.close();
+            if (os != null) {
+                os.close();
             }
         }
     }
